81f68ffcf3740d2afa26b943bf057ca9d32e724c
Bobizaaa12 <165090840+Bobizaaa12@users.noreply.github.com>
2024-03-30 08:33:55 +0000

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,8 +138,8 @@ contextInfo: {
 externalAdReply: {
 title: 'BOBIZA BOT ♥',
 body: "أول بوت واتساب في العالم العربي 💖",
-thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
-sourceUrl: 'https://instagram.com/noureddine_ouafy',
+thumbnailUrl: 'https://telegra.ph/file/39a93b7e3b3c4fb54236d.jpg',
+sourceUrl: 'https://www.instagram.com/ezedine01?igsh=MTdmaXk3dnhnYmgzeg==',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
